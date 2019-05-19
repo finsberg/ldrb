5cb12cb71d9c9025299aa8352d315a8a32457761
Henrik Finsberg <henriknf@simula.no>
2019-05-19 08:15:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,132 @@
-# System imports
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
 import os
 import sys
-import platform
+from shutil import rmtree
 
-from setuptools import setup
+from setuptools import find_packages, setup, Command
 
-# Version number
-major = 0
-minor = 1.1
+# Package meta-data.
+NAME = 'ldrb'
+DESCRIPTION = ('Laplace-Dirichlet Rule-based algorithm for assigning'
+               ' myocardial fiber orientations.')
+URL = 'https://github.com/finsberg/ldrb'
+EMAIL = 'henriknf@simula.no'
+AUTHOR = 'Henrik Finsberg'
+REQUIRES_PYTHON = '>=3.5.0'
+VERSION = '0.1.2'
 
-# REQUIREMENTS = ['h5py==2.8.0',
-                # 'numba==0.40.1',
-                # 'numpy-quaternion']
-# dependency_links = ['git+https://github.com/moble/quaternion#egg=numpy-quaternion']
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'h5py', 'numba', 'numpy-quaternion'
+]
 
-on_rtd = os.environ.get("READTHEDOCS") == "True"
-scripts = []
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
 
-if platform.system() == "Windows" or "bdist_wininst" in sys.argv:
-    # In the Windows command prompt we can't execute Python scripts
-    # without a .py extension. A solution is to create batch files
-    # that runs the different scripts.
-    batch_files = []
-    for script in scripts:
-        batch_file = script + ".bat"
-        f = open(batch_file, "w")
-        f.write('python "%%~dp0\%s" %%*\n' % os.path.split(script)[1])
-        f.close()
-        batch_files.append(batch_file)
-    scripts.extend(batch_files)
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name="ldrb",
-    version="{0}.{1}".format(major, minor),
-    description="""
-      Laplace-Dirichlet Rule-based algorithm for assigning myocardial
-      fiber orientations.
-      """,
-    author="Henrik Finsberg",
-    author_email="henriknf@simula.no",
-    license="LGPL version 3 or later",
-    # install_requires=REQUIREMENTS,
-    # dependency_links=dependency_links,
-    # setup_requires=['numpy>1.7'],
-    packages=["ldrb"]
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "demos"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='LGPL version 3 or later',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
