216a8351695ec8c67d79b4ee8b5fcfb808e06d13
Henrik Finsberg <henriknf@simula.no>
2021-12-17 08:57:04 +0100

    Remomve numba from demoes

--- demos/demo_lv.py
+++ demos/demo_lv.py
@@ -60,12 +60,7 @@ fiber_space = "Lagrange_1"
 
 # Compute the microstructure
 fiber, sheet, sheet_normal = ldrb.dolfin_ldrb(
-    mesh=mesh,
-    fiber_space=fiber_space,
-    ffun=ffun,
-    markers=markers,
-    use_numba=True,  # Set this to True to speed up computations
-    **angles
+    mesh=mesh, fiber_space=fiber_space, ffun=ffun, markers=markers, **angles
 )
 
 # Store the results
